2ec867395a41420f15864a3da4edd840f3a9e3be
Johnson-Gui <64114178+Johnson-Gui@users.noreply.github.com>
2020-04-22 09:55:20 +0800

    Add files via upload

---
+++ logistic.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+X, Y = fetch_openml('mnist_784', version=1, data_home='./scikit_learn_data', return_X_y=True)
+X = X / 255.
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X[::10], Y[::10], test_size=1000)
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+lr=LogisticRegression()
+lr.fit(X_train, Y_train)
+y_predtrain= lr.predict(X_train)
+train_accuracy= np.mean(y_predtrain == Y_train)
+y_predtest = lr.predict(X_test)
+test_accuracy= np.mean(y_predtest ==Y_test)
+
+
+print('Training accuracy: %0.2f%%' % (train_accuracy*100))
+print('Testing accuracy: %0.2f%%' % (test_accuracy*100))
